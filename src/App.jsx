d1cba3f7251db72d8937945959aaac253d089afb
lokteeva <swsw06@mail.ru>
2023-01-08 18:00:47 +0300

    day6

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <header>
       <h1 className='logo'>Встреча с Кришной и Его преданными...</h1>
                         <nav>
-                        <h2  href="#"> </h2>
+                        <h2  href="#">Оставить заявку</h2>
                         <h2  href="#">Контакты</h2>
                         </nav>
       </header>
@@ -83,7 +83,7 @@ function App() {
 
 <div className="bannerSection">
   <div className="banner3">
-      <h3 className='AlmaItalic'>Приглашаем Вас провести свой лучший отпуск в Ашрам Сочи и …</h3>
+      <h3 className='AlmaItalic'>Приглашаем Вас провести свой лучший отпуск<br/> в Ашрам Сочи и …</h3>
   </div>
 </div>
 
@@ -94,31 +94,237 @@ function App() {
 <div className='oo'>
   <div className='ww'>
     <div>.</div>
-    <div >img1</div>
-    <div>img2</div>
-    <div>img3</div>
+    <div><img src="/img111.jpg" className="imgSircl" alt="img"></img></div>
+    <div><img src="/img113.jpg" className="imgSircl" alt="img"></img></div>
+    <div><img src="/img115.jpg" className="imgSircl" alt="img"></img></div>
     <div>.</div>
   </div>
-  <div>
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-    <div>4</div>
-    <div>5</div>
+  <div className='Candara2 '>
+    <div className='MarginForText'>Погрузиться в духовную атмосферу и укрепить свою садхану</div>
+    <div className='MarginForText'>Получить вкус к джаппа-медитации и слушанию священных писаний</div>
+    <div className='MarginForText'>Окунуться в совместное служение и научиться сотрудничеству</div>
+    <div className='MarginForText'>Найти единомышленников и обрести новых друзей</div>
+    <div className='MarginForText'>Эффективно пройти обучение по Вашей образовательной программе в комфортных условиях и располагающей к занятиям атмосфере</div>
   </div>
   <div className='ww2'>
     <div>.</div>
-    <div>img4</div>
+    <div><img src="/img117.jpg" className="imgSircl" alt="img"></img></div>
     <div>.</div>
-    <div>img5</div>
+    <div><img src="/img119.jpg" className="imgSircl" alt="img"></img></div>
     <div>.</div>
   </div>
 </div>
 
+<div className='Candara3 liSection2'>
+<div>
+<img src="/img128.png" className="imgSirclBigPolovina" alt="img"></img>
+</div>
+<div>И получить бонусы: спорт, активный отдых с хорошей компанией на море, в горах или в национальном парке, 
+увлекательные путешествия и оздоровительные программы, детский досуг
+<div className='GridThree'>
+  <div><img src="/img132.jpg" className="imgSirclBig" alt="img"></img></div>
+  <div><img src="/img130.jpg" className="imgSirclBig" alt="img"></img></div>
+  <div><img src="/img134.jpg" className="imgSirclBig" alt="img"></img></div>
+</div>
+</div>
+</div>
+
+<div className="bannerSection">
+  <div className="banner3">
+      <h3 className='AlmaItalic'>О нас:<br/> Ашрам Сочи - «Нью Джаганатха Пури»</h3>
+  </div>
+</div>
+
+<div className="liSection">
+<div>
+  <table class="table">
+  <tbody>
+    <tr>
+      <th scope="row">
+      <img src="/logo.png" className="imgLogo" alt="img"></img>
+      </th>
+      <td className='Candara2'>Ведический Храм
+      </td>
+    </tr>
+    <tr>
+    <th scope="row"><img src="/logo.png" className="imgLogo" alt="img"></img></th>
+      <td className='Candara2'>Брахмачари и Брахмачарини ашрамы</td>
+    </tr>
+    <tr>
+      <th scope="row"><img src="/logo.png" className="imgLogo" alt="img"></img></th>
+      <td className='Candara2'>Ванапрасха ашрам и команда служащих</td>
+    </tr>
+    <tr>
+      <th scope="row">
+      <img src="/logo.png" className="imgLogo" alt="img"></img>
+      </th>
+      <td className='Candara2'>Центр ведической культуры</td>
+    </tr>
+    <tr>
+    <th scope="row"><img src="/logo.png" className="imgLogo" alt="img"></img></th>
+      <td className='Candara2'>Гостевой дом</td>
+    </tr>    
+  </tbody>
+  </table>
+</div> 
+</div>
+
+<div className='GridThree'>
+  <div><img src="/img149.jpg" className="imgSirclBig" alt="img"></img></div>
+  <div><img src="/img147.jpg" className="imgSirclBig" alt="img"></img></div>
+  <div><img src="/img144.jpg" className="imgSirclBig" alt="img"></img></div>
+</div>
+
+<br/>
+<div className="bannerSection">
+  <div className="banner3">
+      <h3 className='AlmaItalic'>Ведический храм </h3><h3><strong className='Candara3White'>Храм Их Светлостей Шри Шри Нитай Гаурасундары<br/>
+и Шри Шри Джаганнатха Баладева и Субхарды</strong></h3>
+  </div>
+</div>
 
 
+<div className="liSection">
+<div>
+  <table class="table">
+  <tbody>
+    <tr>
+      <th scope="row">
+      <img src="/logo.png" className="imgLogo" alt="img"></img>
+      </th>
+      <td className='Candara2'>Высокий стандарт поклонения</td>
+    </tr>
+    <tr>
+    <th scope="row"><img src="/logo.png" className="imgLogo" alt="img"></img></th>
+      <td className='Candara2'>Ежедневные храмовые службы </td>
+    </tr>
+    <tr>
+      <th scope="row"><img src="/logo.png" className="imgLogo" alt="img"></img></th>
+      <td className='Candara2'>Классы по священным писаниям</td>
+    </tr>
+    <tr>
+      <th scope="row">
+      <img src="/logo.png" className="imgLogo" alt="img"></img>
+      </th>
+      <td className='Candara2'>Еженедельные киртаны</td>
+    </tr>
+    <tr>
+    <th scope="row"><img src="/logo.png" className="imgLogo" alt="img"></img></th>
+      <td className='Candara2'>Ведические обряды и церемонии</td>
+    </tr>    
+  </tbody>
+  </table>
+</div> 
+</div>
 
+<div className='GridThree'>
+  <div><img src="/img163.jpg" className="imgSirclBig" alt="img"></img></div>
+  <div><img src="/img161.jpg" className="imgSirclBig" alt="img"></img></div>
+  <div><img src="/img159.jpg" className="imgSirclBig" alt="img"></img></div>
+</div>
+<br/>
 
+<br/>
+<div className="bannerSection">
+  <div className="banner3">
+   <h3>.</h3>
+      <h3 className='AlmaItalic'>Центр ведической культуры</h3> 
+      <h3>.</h3>     
+  </div>
+</div>
+
+<div className="liSection3">
+<div>
+  <table class="table">
+  <tbody>
+    <tr>
+      <th scope="row">
+      <img src="/logo.png" className="imgLogo" alt="img"></img>
+      </th>
+      <td className='Candara2'>Образовательные проекты</td>
+    </tr>
+    <tr>
+    <th scope="row"><img src="/logo.png" className="imgLogo" alt="img"></img></th>
+      <td className='Candara2'>Фестивали и ретриты </td>
+    </tr>
+    <tr>
+      <th scope="row"><img src="/logo.png" className="imgLogo" alt="img"></img></th>
+      <td className='Candara2'>Городские мероприятия</td>
+    </tr>
+    <tr>
+      <th scope="row">
+      <img src="/logo.png" className="imgLogo" alt="img"></img>
+      </th>
+      <td className='Candara2'>Проповеднические программы</td>
+    </tr>
+    <tr>
+    <th scope="row"><img src="/logo.png" className="imgLogo" alt="img"></img></th>
+      <td className='Candara2'>Благотворительные проекты:<br/> «Пища жизни» и др.</td>
+    </tr>    
+  </tbody>
+  </table>
+</div> 
+
+<div className='Grid2x2'>
+  <div><img src="/img173.jpg" className="imgCub" alt="img"></img></div>
+  <div><img src="/img175.jpg" className="imgCub" alt="img"></img></div>
+  <div><img src="/img172.jpg" className="imgCub" alt="img"></img></div>
+  <div><img src="/img174.jpg" className="imgCub" alt="img"></img></div>
+</div>
+</div>
+
+<br/>
+<div className="bannerSection">
+  <div className="banner3">
+   <h3>.</h3>
+      <h3 className='AlmaItalic'>Гостевой дом</h3> 
+      <h3>.</h3>     
+  </div>
+</div>
+
+<div className="liSection">
+<div>
+  <table class="table">
+  <tbody>
+    <tr>
+      <th scope="row">
+      <img src="/logo.png" className="imgLogo" alt="img"></img>
+      </th>
+      <td className='Candara2'><strong className='Candara3'>Гостевые номера:</strong> 2х, 3х, 4х, 6ти –местные</td>
+    </tr>
+    <tr>
+    <th scope="row"><img src="/logo.png" className="imgLogo" alt="img"></img></th>
+      <td className='Candara2'>новое общение, обучение и <strong className='Candara3'>развитие</strong> </td>
+    </tr>
+    <tr>
+      <th scope="row"><img src="/logo.png" className="imgLogo" alt="img"></img></th>
+      <td className='Candara2'> спорт или активный отдых на <strong className='Candara3'>море</strong>, в <strong className='Candara3'>горах</strong></td>
+    </tr>
+    <tr>
+      <th scope="row">
+      <img src="/logo.png" className="imgLogo" alt="img"></img>
+      </th>
+      <td className='Candara2'>комфортное комплексное <strong className='Candara3'>оздоровление</strong></td>
+    </tr>
+    <tr>
+    <th scope="row"><img src="/logo.png" className="imgLogo" alt="img"></img></th>
+      <td className='Candara2'>живые эмоции и насыщенные <strong className='Candara3'>путешествия</strong></td>
+    </tr>
+    <tr>
+      <th scope="row">
+      <img src="/logo.png" className="imgLogo" alt="img"></img>
+      </th>
+      <td className='Candara2'>тихий семейный отдых и интересный <strong className='Candara3'>детский досуг</strong></td>
+    </tr>
+    <tr>
+    <th scope="row"><img src="/logo.png" className="imgLogo" alt="img"></img></th>
+      <td className='Candara2'>все или более одного варианта, такой мой 
+<strong className='Candara3'> отпуск лучший!</strong></td>
+    </tr>
+  </tbody>
+  </table>
+</div> 
+</div>
 
 
 </div>                 
